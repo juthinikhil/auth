9734ec5fc288c30a7ccc7a123ade6a50052a9bd7
NIKHIL <jvss.rao.97@gmail.com>
2024-01-27 16:00:33 +0530

    server json deleted added server js

---
+++ server.js
@@ -0,0 +1,48 @@
+﻿// .. other imports
+
+const { auth } = require("express-oauth2-jwt-bearer");
+const authConfig = require("./auth_config.json");
+const express = require("express");
+const { join } = require("path");
+const app = express();
+
+// create the JWT middleware
+const checkJwt = auth({
+    audience: authConfig.audience,
+    issuerBaseURL: `https://${authConfig.domain}`
+});
+// Serve static assets from the /public folder
+app.use(express.static(join(__dirname, "public")));
+
+// Endpoint to serve the configuration file
+app.get("/auth_config.json", (req, res) => {
+    res.sendFile(join(__dirname, "auth_config.json"));
+});
+
+// Serve the index page for all other requests
+app.get("/*", (_, res) => {
+    res.sendFile(join(__dirname, "index.html"));
+});
+
+// Listen on port 3000
+app.listen(3000, () => console.log("Application running on port 3000"));
+
+//Protected endpoint
+app.get("/api/external", checkJwt, (req, res) => {
+    res.send({
+        msg: "Your access token was successfully validated!"
+    });
+});
+
+// ..error handler
+
+app.use(function(err, req, res, next) {
+    if (err.name === "UnauthorizedError") {
+        return res.status(401).send({ msg: "Invalid token" });
+    }
+
+    next(err, req, res);
+});
+
+//..
+
